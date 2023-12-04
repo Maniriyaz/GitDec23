e2632418307a2e79518202c386f24e28e5f6e201
Maniriyaz <manipanda4698@gmail.com>
2023-12-03 22:48:19 -0800

    Code will done

---
+++ src/org/gt/EmpDetails.java
@@ -0,0 +1,17 @@
+package org.gt;
+
+public class EmpDetails {
+	
+	private void empId() {
+		System.out.println("Employee id is 200");
+	}
+	
+	public static void main(String[] args) {
+		
+		EmpDetails e = new EmpDetails();
+		
+		e.empId();
+		
+	}
+
+}
